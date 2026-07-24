5320435ce78e2bec243930ff26846e7d49f6f68e
namtae01 <46198324+JoYuKang@users.noreply.github.com>
2026-07-24 16:00:45 +0900

    chore: configure local H2 and MySQL profile-based datasource

--- src/main/java/com/kang/ecommercedataplatform/EcommerceDataPlatformApplication.java
+++ src/main/java/com/kang/ecommercedataplatform/EcommerceDataPlatformApplication.java
@@ -2,13 +2,10 @@ package com.kang.ecommercedataplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class EcommerceDataPlatformApplication {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(EcommerceDataPlatformApplication.class, args);
 	}
-
 }
